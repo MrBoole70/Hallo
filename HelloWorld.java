3875df9aae2ea32848ac43c9731afe7f2f1cda4b
MrBoole70 <49103122+MrBoole70@users.noreply.github.com>
2019-03-30 19:18:33 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+// Hello World!
+// Prima App JAVA
+// By Alessandro Michele Di Santo. 30/03/2019 ore 13:45
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World!");
+        System.out.println("By Alessandro Michele Di Santo. "
+        		+ "30/03/2019 ore 13:45 "
+        		+ "Prima App Java");
+	}
+
+}
